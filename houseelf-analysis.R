4519a4052d8cf513f90814744e1e53a19319fdba
Kodiak Hengstebeck <khengste@ufl.edu>
2016-11-18 12:54:47 -0500

    renamed file again

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -7,8 +7,8 @@
 houseelf_data = read.csv("Data/houseelf-earlength-dna-data.csv")
 
 # Exercise 4
-houseelf_data_1 = read.csv("Data/houseelf-earlength-dna-data1.csv")
-#I changed the name of the file and committed the changes to git
+houseelf_data_1 = read.csv("Data/houseelf-earlength-dna-data_1_.csv")
+#I changed the name of the file and committed the changes to git in a single commit (i did it wrong the first time, so there are a few extra commits in there)
 
 # Exercise 5
 #5.1
@@ -16,3 +16,8 @@ houseelf_data_1 = read.csv("Data/houseelf-earlength-dna-data1.csv")
 
 #5.2
 # I connected my local repository to the github repository by pasting the 2 lines of code provided by github into 'shell'
+
+# Exercise 6
+library(stringr)
+str_to_lower(houseelf_data_1)
+
