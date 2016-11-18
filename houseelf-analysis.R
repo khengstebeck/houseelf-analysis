3c83695513603f51f7ec75c0fb2459365089ae4d
Kodiak Hengstebeck <khengste@ufl.edu>
2016-11-18 12:20:19 -0500

    I changed the name so that files can be numbered, which makes organizing easier

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -5,3 +5,14 @@
 
 # Exercise 3
 houseelf_data = read.csv("Data/houseelf-earlength-dna-data.csv")
+
+# Exercise 4
+houseelf_data_1 = read.csv("Data/houseelf-earlength-dna-data_1.csv")
+#I changed the name of the file and committed the changes
+
+# Exercise 5
+#5.1
+# I created a public repository called "houseelf-analysis"
+
+#5.2
+# I connected my local repository to the github repository by pasting the 2 lines of code provided by github into 'shell'
